1c36fe58fafd5f00befcca3b42b0429dca635a1f
goergen95 <darius2402@web.de>
2019-07-22 12:41:56 +0200

    erase uneede js

--- assets/js/enlargeimg.js
+++
@@ -1,9 +0,0 @@
-function resize(direction) {
-  var delta = 100 * direction;
-
-  var element = document.getElementById('img');
-  var positionInfo = element.getBoundingClientRect();
-
-  element.style.width = positionInfo.width+delta+'px';
-  element.style.height = positionInfo.height+delta+'px';
-}
